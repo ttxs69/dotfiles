5f0c583552296e8407ae87c7de9d0fba98c3d73c
sarace <wshuai@proton.me>
2023-06-04 15:43:07 +0800

    add org agenda files

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/daily/org/goals.org"))
  '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
