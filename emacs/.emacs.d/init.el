aea41ff2dd014e34bb247fa86848a0c96856f33b
sarace <wshuai@proton.me>
2023-06-04 16:11:30 +0800

    add global key bindings for org;install company mode;enable linum mode
    Signed-off-by: sarace <wshuai@proton.me>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,8 +4,18 @@
                          ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize) ;; You might already have this line
+
 ;; close backup
 (setq make-backup-files nil)
+
+;; global key bindings
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; global enabled mode
+(global-company-mode)
+(global-linum-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -13,7 +23,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/daily/org/"))
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(company magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
