f717df28bfad09e1e83c94b8adba764e290ffdb4
sarace <sarace@huisa.win>
2024-05-12 13:54:18 +0800

    feat: add evil-mode org-modern and magit packages to emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -48,12 +48,20 @@
   :init
   (load-theme 'tango-dark))
 
+(use-package evil
+  :config
+  (evil-mode t))
+
+(use-package org
+  :init
+  (global-org-modern-mode t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package company)))
+ '(package-selected-packages '(magit org-modern evil use-package company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
