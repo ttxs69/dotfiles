f1dc54ca57db2f577e3218e3433866066142fc75
sarace <sarace@huisa.win>
2024-05-18 17:24:06 +0800

    delete evil mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -48,10 +48,6 @@
   :init
   (load-theme 'tango-dark))
 
-(use-package evil
-  :config
-  (evil-mode t))
-
 (use-package org
   :init
   (global-org-modern-mode t))
@@ -61,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit org-modern evil use-package company)))
+ '(package-selected-packages '(magit org-modern use-package company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
