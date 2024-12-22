bcdf0f999de375e166a9903d61a1a262de772007
sarace <sarace@huisa.win>
2024-12-22 12:09:33 +0800

    feat(nvim): add lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,70 +18,71 @@ return {
 				-- and will be called for each installed server that doesn't have
 				-- a dedicated handler.
 				function (server_name) -- default handler (optional)
-					require("lspconfig")[server_name].setup {
-				}
+					require("lspconfig")[server_name].setup {}
 				end,
 				-- Next, you can provide a dedicated handler for specific servers.
 				-- For example, a handler override for the `rust_analyzer`:
 				-- ["rust_analyzer"] = function ()
 				--     require("rust-tools").setup {}
 				-- end
-				--  This function gets run when an LSP attaches to a particular buffer.
-				--    That is to say, every time a new file is opened that is associated with
-				--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-				--    function will be executed to configure the current buffer
-				vim.api.nvim_create_autocmd('LspAttach', {
-					group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-					callback = function(event)
-						-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-						-- to define small helper and utility functions so you don't have to repeat yourself.
-						--
-						-- In this case, we create a function that lets us more easily define mappings specific
-						-- for LSP related items. It sets the mode, buffer and description for us each time.
-						local keymap = function(keys, func, desc, mode)
-							mode = mode or 'n'
-							vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-						end
-
-						-- Jump to the definition of the word under your cursor.
-						--  This is where a variable was first declared, or where a function is defined, etc.
-						--  To jump back, press <C-t>.
-						keymap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-						-- Find references for the word under your cursor.
-						keymap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-						-- Jump to the implementation of the word under your cursor.
-						--  Useful when your language has ways of declaring types without an actual implementation.
-						keymap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-						-- Jump to the type of the word under your cursor.
-						--  Useful when you're not sure what type a variable is and you want to see
-						--  the definition of its *type*, not where it was *defined*.
-						keymap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-						-- Fuzzy find all the symbols in your current document.
-						--  Symbols are things like variables, functions, types, etc.
-						keymap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-						-- Fuzzy find all the symbols in your current workspace.
-						--  Similar to document symbols, except searches over your entire project.
-						keymap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-						-- Rename the variable under your cursor.
-						--  Most Language Servers support renaming across files, etc.
-						keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-						-- Execute a code action, usually your cursor needs to be on top of an error
-						-- or a suggestion from your LSP for this to activate.
-						keymap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-						-- WARN: This is not Goto Definition, this is Goto Declaration.
-						--  For example, in C this would take you to the header.
-						keymap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-					end
-				})
 			}
+
+			--  This function gets run when an LSP attaches to a particular buffer.
+			--    That is to say, every time a new file is opened that is associated with
+			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+			--    function will be executed to configure the current buffer
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+				callback = function(event)
+					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+					-- to define small helper and utility functions so you don't have to repeat yourself.
+					--
+					-- In this case, we create a function that lets us more easily define mappings specific
+					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local keymap = function(keys, func, desc, mode)
+						mode = mode or 'n'
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+					end
+
+					-- Jump to the definition of the word under your cursor.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
+					keymap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+					-- Find references for the word under your cursor.
+					keymap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+					-- Jump to the implementation of the word under your cursor.
+					--  Useful when your language has ways of declaring types without an actual implementation.
+					keymap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+					-- Jump to the type of the word under your cursor.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
+					keymap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+					-- Fuzzy find all the symbols in your current document.
+					--  Symbols are things like variables, functions, types, etc.
+					keymap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+					-- Fuzzy find all the symbols in your current workspace.
+					--  Similar to document symbols, except searches over your entire project.
+					keymap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+					-- Rename the variable under your cursor.
+					--  Most Language Servers support renaming across files, etc.
+					keymap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+					-- Execute a code action, usually your cursor needs to be on top of an error
+					-- or a suggestion from your LSP for this to activate.
+					keymap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+					-- WARN: This is not Goto Definition, this is Goto Declaration.
+					--  For example, in C this would take you to the header.
+					keymap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+				end
+			})
+
 		end,
 	}
 }
