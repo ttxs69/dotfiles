4cb101b9aa05eb25a3bbd4a816ac7193805daaca
sarace <sarace@huisa.win>
2024-12-26 12:31:53 +0800

    fix: enable nvim-cmp when CmdlineEnter and format file

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,7 +2,7 @@ return {
 	{ -- optional cmp completion source for require statements and module annotations
 		"hrsh7th/nvim-cmp",
 		-- load cmp on InsertEnter
-		event = "InsertEnter",
+		event = { "InsertEnter", "CmdlineEnter" },
 		-- these dependencies will only be loaded when cmp loads
 		-- dependencies are always lazy-loaded unless specified otherwise
 		dependencies = {
@@ -26,14 +26,14 @@ return {
 		end,
 		config = function()
 			-- Set up nvim-cmp.
-			local cmp = require'cmp'
+			local cmp = require("cmp")
 			vim.print("configing nvim-cmp")
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
 						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
@@ -44,25 +44,22 @@ return {
 					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
-				sources = cmp.config.sources(
-				{
-					{ name = 'nvim_lsp' },
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
 					-- { name = 'vsnip' }, -- For vsnip users.
-					{ name = 'luasnip' }, -- For luasnip users.
+					{ name = "luasnip" }, -- For luasnip users.
 					-- { name = 'ultisnips' }, -- For ultisnips users.
 					-- { name = 'snippy' }, -- For snippy users.
-				},
-				{
-					{ name = 'buffer' },
-					{ name = 'path' },
-				}
-				)
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
 			})
 
 			-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -74,36 +71,37 @@ return {
 					{ name = 'buffer' },
 				})
 			})
-			require("cmp_git").setup() ]]-- 
+			require("cmp_git").setup() ]]
+			--
 
 			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ '/', '?' }, {
+			cmp.setup.cmdline({ "/", "?" }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = 'buffer' }
-				}
+					{ name = "buffer" },
+				},
 			})
 
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(':', {
+			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = 'path' }
+					{ name = "path" },
 				}, {
-					{ name = 'cmdline' }
+					{ name = "cmdline" },
 				}),
-				matching = { disallow_symbol_nonprefix_matching = false }
+				matching = { disallow_symbol_nonprefix_matching = false },
 			})
 
 			-- Set up lspconfig.
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-			require('lspconfig')['lua_ls'].setup {
-				capabilities = capabilities
-			}
-			require('lspconfig')['rust_analyzer'].setup {
-				capabilities = capabilities
-			}
-		end
-  },
+			require("lspconfig")["lua_ls"].setup({
+				capabilities = capabilities,
+			})
+			require("lspconfig")["rust_analyzer"].setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
