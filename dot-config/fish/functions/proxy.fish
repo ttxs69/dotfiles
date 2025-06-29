2c4da0bc4bf8b2791bd0c06e801724456dfc41a9
sarace <sarace@huisa.win>
2025-06-29 15:35:54 +0800

    fix: fix fish shell env for proxy

--- dot-config/fish/functions/proxy.fish
+++ dot-config/fish/functions/proxy.fish
@@ -1,12 +1,12 @@
 function proxy
-    set -x https_proxy http://127.0.0.1:7890
-    set -x http_proxy http://127.0.0.1:7890
-    set -x all_proxy socks5://127.0.0.1:7890
+    set -gx https_proxy http://127.0.0.1:7890
+    set -gx http_proxy http://127.0.0.1:7890
+    set -gx all_proxy socks5://127.0.0.1:7890
 end
 
 function unproxy
-    set -e https_proxy
-    set -e http_proxy
-    set -e all_proxy
+    set -ge https_proxy
+    set -ge http_proxy
+    set -ge all_proxy
 end
 
