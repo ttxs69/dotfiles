6c169e0b324a81effb4854016c7e806da8841e74
sarace <sarace@huisa.win>
2025-10-12 15:27:19 +0800

    feat: remove dabbrev

--- dot-config/emacs/init.el
+++ dot-config/emacs/init.el
@@ -45,18 +45,6 @@
   (add-hook 'completion-at-point-functions #'cape-elisp-block)
   )
 
-;; Use Dabbrev with Corfu!
-(use-package dabbrev
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
-  :config
-  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
-  (add-to-list 'dabbrev-ignored-buffer-modes 'authinfo-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
-
 (use-package key-chord
   :ensure t
   :after evil
